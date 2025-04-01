bb95ea0b461c89e4688cc11652fe46b93e298f08
Austin Orphan <austin.orphan1@gmail.com>
2025-04-01 10:41:39 -0700

    WIP

--- austin-orphan.github.io/src/Nav.jsx
+++ austin-orphan.github.io/src/Nav.jsx
@@ -3,10 +3,10 @@ function Nav() {
         <nav>
             <a href="#home">Home</a>
             <a href="#about">About</a>
-            <a href="#projects">Data Structures & Algorithms - Cheat Sheet</a>
-            <a href="#contact">Lean Software Development</a>
+            <a href="#dsa">Data Structures & Algorithms - Cheat Sheet</a>
+            <a href="#lsd">Lean Software Development</a>
         </nav>
     );
 }
 
-export default Nav;
+export default Nav; 
